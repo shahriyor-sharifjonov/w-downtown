478fb98585d2c15cedc16552a63b932883d06cc0
Shahriyor <shahriyordeveloping@gmail.com>
2023-01-24 10:08:01 +0500

    init

--- src/js/modules/canvas.js
+++ src/js/modules/canvas.js
@@ -1,9 +1,17 @@
 import CanvasScrollClip from "canvas-scroll-clip";
 
 export function init() {
-    new CanvasScrollClip(document.querySelector('.about__canvas'), {
-        framePath: "img/000000.png",
-        frameCount: 100,
-        scrollArea: window.innerHeight * 2
-    })
+    if(window.innerWidth <= 576){
+        new CanvasScrollClip(document.querySelector('.about__canvas'), {
+            framePath: "img/about-mob/00000.png",
+            frameCount: 119,
+            scrollArea: window.innerHeight * 2
+        })
+    }else {
+        new CanvasScrollClip(document.querySelector('.about__canvas'), {
+            framePath: "img/about-desktop/000000.png",
+            frameCount: 100,
+            scrollArea: window.innerHeight * 2
+        })
+    }
 }
